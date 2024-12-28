16cd59d5cdf5214bfc9e65903e44a28d3e717f6a
Stefanos Makrysopoulos <smakrysopoulos@novibet.com>
2024-12-28 10:19:06 +0200

    added logger

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ mod models;
 mod services;
 mod routes;
 
-use actix_web::{get, web::Data, App, HttpResponse, HttpServer, Responder};
+
+use actix_web::{get, middleware::Logger, web::Data, App, HttpResponse, HttpServer, Responder};
 use routes::build_metadata_route::{create_metadata, get_metadata};
 use services::db::Database;
 
@@ -15,10 +16,12 @@ async fn hello() -> impl Responder{
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
     let db = Database::init().await;
     let db_data = Data::new(db);
     HttpServer::new(move || App::new().app_data(db_data.clone())
+    .wrap(Logger::default())
     .service(hello)
     .service(create_metadata)
     .service(get_metadata)
